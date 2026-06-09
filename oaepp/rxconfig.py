e06cd567940757b6dae473fde803e75712402d22
周鹏 <2606351658@qq.com>
2026-06-09 16:23:22 +0800

    Update rxconfig.py

--- oaepp/rxconfig.py
+++ oaepp/rxconfig.py
@@ -7,6 +7,7 @@ config = rx.Config(
     app_name="oaepp",
     app_module_import="app",
     frontend_port=int(os.environ.get("REFLEX_FRONTEND_PORT", "8000")),
+    backend_port=int(os.environ.get("REFLEX_FRONTEND_PORT", "8000")),
     backend_host=os.environ.get("REFLEX_BACKEND_HOST", "0.0.0.0"),
     api_url=os.environ.get(
         "REFLEX_API_URL",
